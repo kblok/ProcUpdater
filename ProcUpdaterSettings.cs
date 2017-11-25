3d962583449de191edfd23247b219f9e01587556
Darío Kondratiuk <dariokondratiuk@gmail.com>
2017-11-25 17:45:58 -0300

    Config loader

---
+++ ProcUpdaterSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ProcUpdater
+{
+    public class ProcUpdaterSettings
+    {
+        public static string SectionKey => "ProcUpdater";
+        public static string ConnectionStringKey => "ProcUpdater:ConnectionString";
+        public static string StoredProceduresPathKey => "ProcUpdater:StoredProceduresPath";
+        public static string FileWatchKey => "ProcUpdater:FileWatch";
+        public static string StayAliveKey => "ProcUpdater:StayAlive";
+
+        public static string ExitKeyword => "quit";
+        public static string RunKeyword => "run";
+
+        public string ConnectionString { get; set; }
+        public string StoredProceduresPath { get; set; }
+        public bool FileWatch { get; set; }
+        public bool StayAlive { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace ProcUpdater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var arguments = LoadArguments(args);
+
+                if (arguments != null && IsValid(arguments))
+                {
+
+                }
+                else
+                {
+                    PrintHelp();
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("ERROR");
+                Console.WriteLine(ex.GetType().Name);
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                Console.ReadLine();
+            }
+        }
+
+        private static bool IsValid(ProcUpdaterSettings arguments)
+        {
+            
+            return 
+                !string.IsNullOrEmpty(arguments.ConnectionString) &&
+                !string.IsNullOrEmpty(arguments.StoredProceduresPath);
+        }
+
+        private static void PrintHelp()
+        {
+            Console.WriteLine("Usage: ProcUpdater.exe [options]\n\n");
+            Console.WriteLine("Options");
+            Console.WriteLine("-conn:\tConnection String (REQUIRED)");
+            Console.WriteLine("-path:\tPath where the stored procedures are located (REQUIRED)");
+            Console.WriteLine("-alive:\tKeep the app open");
+            Console.WriteLine("-watch:\tWatch changes in the path and auto-updated procedures");
+            Console.WriteLine("\n\nIn-app commands");
+            Console.WriteLine("run:\tRuns the procedure update task");
+            Console.WriteLine("quit:\tQuits the application");
+        }
+
+
+        private static ProcUpdaterSettings LoadArguments(string[] commandArgs)
+        {
+            try
+            {
+                //Setup the configuration sources
+                var builder = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .AddCommandLine(commandArgs, new Dictionary<string, string>
+                    {
+                    {"-conn", ProcUpdaterSettings.ConnectionStringKey},
+                    {"-path", ProcUpdaterSettings.StoredProceduresPathKey},
+                    {"-alive", ProcUpdaterSettings.StayAliveKey},
+                    {"-watch", ProcUpdaterSettings.FileWatchKey}
+                    });
+
+                var configuration = builder.Build();
+                var procArguments = new ProcUpdaterSettings();
+                configuration.GetSection("ProcUpdater").Bind(procArguments);
+
+                return procArguments;
+            }
+            catch(FormatException)
+            {
+                return null;
+            }
+
+        }
+    }
+}
