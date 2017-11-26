a65f95408a9c1b6990b889ce0e6e139026e89761
Darío Kondratiuk <dariokondratiuk@gmail.com>
2017-11-26 18:08:23 -0300

    Fix version

--- ProcUpdaterSettings.cs
+++ ProcUpdaterSettings.cs
@@ -4,18 +4,20 @@ namespace ProcUpdater
 {
     public class ProcUpdaterSettings
     {
-        public static string SectionKey => "ProcUpdater";
-        public static string ConnectionStringKey => "ProcUpdater:ConnectionString";
-        public static string StoredProceduresPathKey => "ProcUpdater:StoredProceduresPath";
-        public static string FileWatchKey => "ProcUpdater:FileWatch";
-        public static string StayAliveKey => "ProcUpdater:StayAlive";
+        public const string SectionKey = "ProcUpdater";
+        public const string ConnectionStringKey = "ProcUpdater:ConnectionString";
+        public const string StoredProceduresPathKey = "ProcUpdater:StoredProceduresPath";
+        public const string FileWatchKey = "ProcUpdater:FileWatch";
+        public const string StayAliveKey = "ProcUpdater:StayAlive";
+        public const string VerboseKey = "ProcUpdater:Verbose";
 
-        public static string ExitKeyword => "quit";
-        public static string RunKeyword => "run";
+        public const string ExitKeyword = "quit";
+        public const string RunKeyword = "run";
 
         public string ConnectionString { get; set; }
         public string StoredProceduresPath { get; set; }
         public bool FileWatch { get; set; }
         public bool StayAlive { get; set; }
+        public bool Verbose { get; set; }
     }
 }
--- Program.cs
+++ Program.cs
@@ -4,27 +4,52 @@ using System.IO;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+using System.Text.RegularExpressions;
+using System.Data.SqlClient;
 
 namespace ProcUpdater
 {
     class Program
     {
+        static ProcUpdaterSettings _settings;
+
         static void Main(string[] args)
         {
             try
             {
-                var arguments = LoadArguments(args);
+                _settings = LoadArguments(args);
 
-                if (arguments != null && IsValid(arguments))
+                if (_settings != null && IsValid(_settings))
                 {
+                    RunDirectoryScripts(new DirectoryInfo(_settings.StoredProceduresPath));
 
+                    //Setup Watcher
+                    if (_settings.FileWatch)
+                    {
+                        SetupWatcher();
+                    }
+                    else if (_settings.StayAlive)
+                    {
+                        while (true)
+                        {
+                            Console.WriteLine("Waiting for new commands (run or quit)");
+                            switch (Console.ReadLine())
+                            {
+                                case ProcUpdaterSettings.RunKeyword:
+                                    RunDirectoryScripts(new DirectoryInfo(_settings.StoredProceduresPath));
+                                    break;
+                                case ProcUpdaterSettings.ExitKeyword:
+                                    return;
+                            }
+                        }
+                    }
                 }
                 else
                 {
                     PrintHelp();
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Console.WriteLine("ERROR");
                 Console.WriteLine(ex.GetType().Name);
@@ -34,14 +59,163 @@ namespace ProcUpdater
             }
         }
 
-        private static bool IsValid(ProcUpdaterSettings arguments)
+        /// <summary>
+        /// Setups the watcher.
+        /// </summary>
+        private static void SetupWatcher()
         {
-            
-            return 
-                !string.IsNullOrEmpty(arguments.ConnectionString) &&
-                !string.IsNullOrEmpty(arguments.StoredProceduresPath);
+            var watcher = new FileSystemWatcher();
+            watcher.Path = _settings.StoredProceduresPath;
+            watcher.Filter = "*.sql";
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite
+                | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            watcher.IncludeSubdirectories = true;
+
+            watcher.Created += FileWatcher_FileChanged;
+            watcher.Changed += FileWatcher_FileChanged;
+            watcher.Renamed += FileWatcher_FileChanged;
+
+            watcher.EnableRaisingEvents = true;
+            Console.WriteLine("Waiting for changes");
+            Console.ReadLine();
         }
 
+        private static void FileWatcher_FileChanged(object sender, FileSystemEventArgs e)
+        {
+            try
+            {
+                RunFile(new FileInfo(e.FullPath));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed processing {e.FullPath}: {ex.Message}");
+            }
+        }
+
+        /// <summary>
+        /// Runs all the produres inside the directory
+        /// </summary>
+        /// <param name="directoryInfo">Directory where the files are located.</param>
+        private static void RunDirectoryScripts(DirectoryInfo directoryInfo)
+        {
+            foreach (var directory in directoryInfo.GetDirectories())
+            {
+                RunDirectoryScripts(directory);
+            }
+            foreach (var file in directoryInfo.GetFiles("*.sql"))
+            {
+                RunFile(file);
+            }
+        }
+
+        /// <summary>
+        /// Updates the procedure
+        /// </summary>
+        /// <param name="file">File with the procedure.</param>
+        private static void RunFile(FileInfo file)
+        {
+            if (_settings.Verbose)
+            {
+                Console.Write($"Processing script: {file.FullName}.....");
+            }
+
+            //Get the string for the file
+            string script;
+            var fileStream = new FileStream(file.FullName, FileMode.Open);
+            using (var reader = new StreamReader(fileStream))
+            {
+                script = reader.ReadToEnd();
+            }
+
+            //Check if the string has a proc inside
+            if (script.Contains("PROCEDURE"))
+            {
+                //Curate the procedure
+
+                //We don't support multiple batchs, a.k.a. GO
+                //So we remove all the GOs we find
+                script = Regex.Replace(script, "^Go[$\\n]*", string.Empty, RegexOptions.IgnoreCase | RegexOptions.Multiline);
+                //We replace "CREATE PROCEDURE" for "CREATE OR ALTER PROCEDURE"
+                script = Regex.Replace(script, "CREATE *PROCEDURE", "CREATE OR ALTER PROCEDURE", RegexOptions.IgnoreCase);
+
+                //Perform update
+                if (RunScript(script))
+                {
+                    if (_settings.Verbose)
+                    {
+                        Console.Write("OK\n");
+                    }
+                }
+                else
+                {
+                    if (_settings.Verbose)
+                    {
+                        Console.Write("FAILED\n");
+                    }
+                }
+            }
+            else
+            {
+                if (_settings.Verbose)
+                {
+                    Console.Write("No procedure found\n");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Runs the script.
+        /// </summary>
+        /// <param name="script">Curated script.</param>
+        private static bool RunScript(string script)
+        {
+            SqlConnection conn = null;
+            var result = false;
+
+            try
+            {
+                conn = new SqlConnection(_settings.ConnectionString);
+                conn.Open();
+                var cmd = new SqlCommand(script, conn);
+                cmd.CommandType = System.Data.CommandType.Text;
+                cmd.ExecuteNonQuery();
+                result = true;
+            }
+            catch (Exception ex)
+            {
+                if (_settings.Verbose)
+                {
+                    Console.Write($"{ex.Message}\t");
+                }
+            }
+            finally
+            {
+                if (conn?.State == System.Data.ConnectionState.Open)
+                {
+                    conn.Close();
+                }
+                conn?.Dispose();
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Checks that the all the arguments are valid
+        /// </summary>
+        /// <returns><c>true</c>, if all arguments are valid <c>false</c> otherwise.</returns>
+        /// <param name="arguments">User Settings</param>
+        private static bool IsValid(ProcUpdaterSettings settings)
+        {
+            return
+                !string.IsNullOrEmpty(settings.ConnectionString) &&
+                !string.IsNullOrEmpty(settings.StoredProceduresPath) &&
+                new DirectoryInfo(settings.StoredProceduresPath).Exists;
+        }
+
+        /// <summary>
+        /// Prints help message in case of an errr or missing arguments.
+        /// </summary>
         private static void PrintHelp()
         {
             Console.WriteLine("Usage: ProcUpdater.exe [options]\n\n");
@@ -50,12 +224,17 @@ namespace ProcUpdater
             Console.WriteLine("-path:\tPath where the stored procedures are located (REQUIRED)");
             Console.WriteLine("-alive:\tKeep the app open");
             Console.WriteLine("-watch:\tWatch changes in the path and auto-updated procedures");
+            Console.WriteLine("-verbose:\tVerbose mode");
             Console.WriteLine("\n\nIn-app commands");
             Console.WriteLine("run:\tRuns the procedure update task");
             Console.WriteLine("quit:\tQuits the application");
         }
 
-
+        /// <summary>
+        /// Creates a <see cref="ProcUpdaterSettings"/> based on command line arguments and json config file
+        /// </summary>
+        /// <returns>ARguments set by the user</returns>
+        /// <param name="commandArgs">Command arguments received by the main method</param>
         private static ProcUpdaterSettings LoadArguments(string[] commandArgs)
         {
             try
@@ -69,7 +248,8 @@ namespace ProcUpdater
                     {"-conn", ProcUpdaterSettings.ConnectionStringKey},
                     {"-path", ProcUpdaterSettings.StoredProceduresPathKey},
                     {"-alive", ProcUpdaterSettings.StayAliveKey},
-                    {"-watch", ProcUpdaterSettings.FileWatchKey}
+                    {"-watch", ProcUpdaterSettings.FileWatchKey},
+                    {"-verbose", ProcUpdaterSettings.VerboseKey}
                     });
 
                 var configuration = builder.Build();
@@ -78,7 +258,7 @@ namespace ProcUpdater
 
                 return procArguments;
             }
-            catch(FormatException)
+            catch (FormatException)
             {
                 return null;
             }
